8fb10c52282e9e031b47c71e6e18a62d9187a4b4
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-06-30 22:35:07 +0300

    Create Button.js

---
+++ src/components/Button/Button.js
@@ -0,0 +1,7 @@
+const Button = ({onClickBtnIncrement}) => {
+    return ( <div>
+        <button className="Button" type="buttton" onClick={onClickBtnIncrement}>Load more</button>
+    </div> );
+}
+ 
+export default Button;
