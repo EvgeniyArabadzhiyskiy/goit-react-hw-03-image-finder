2644f0debfaf5e06048387164f820b3b6733bf54
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-01 18:48:47 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import { Component } from 'react';
 import axios from 'axios';
+import { Circles } from 'react-loader-spinner';
+import { Component } from 'react';
 import Button from './Button/Button';
 // import Box from './Box/Box';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -8,60 +9,111 @@ import Modal from './Modal/Modal';
 
 class App extends Component {
   state = {
+    page: 1,
     query: '',
     articles: [],
-    page: 1,
-    modalOpen: false,
-    largeUrl: null
+    showModal: false,
+    activIndex: null,
+    // isloading: false,
+    status: 'idle',
   };
 
-
   async componentDidUpdate(prevProps, prevState) {
-
-
-    if (this.state.query !== prevState.query || this.state.page !== prevState.page) {
-
-      const response = await axios(
-        `https://pixabay.com/api/?key=26298929-dc8db63efad38f2c4177a32d6&q=${this.state.query}&image_type=photo&page=${this.state.page}&per_page=12`
-      );
-
-      this.setState(prevState => ({articles: [...prevState.articles, ...response.data.hits]}))
+    if (
+      this.state.query !== prevState.query ||
+      this.state.page !== prevState.page
+    ) {
+      // this.setState({ isloading: true });
+      this.setState({ status: 'pending' });
+      try {
+        const response = await axios(
+          `https://pixabay.com/api/?key=26298929-dc8db63efad38f2c4177a32d6&q=${this.state.query}&image_type=photo&page=${this.state.page}&per_page=12`
+        );
+        this.setState(prevState => ({
+          articles: [...prevState.articles, ...response.data.hits],
+          status: 'resolved'
+        }));
+      } catch (error) {}
+      //  finally { this.setState({ isloading: false });}
     }
   }
 
   handleFormSubmit = text => {
-    this.setState({ query: text, page: 1, articles: [] });
-    
+    this.setState({ query: text.trim(), page: 1, articles: [] });
   };
 
-  handlebtnIncrement  = (e) => {
-    this.setState(prevState => ({page: prevState.page + 1}))
- };
+  handleBtnIncrement = e => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
 
- handleImageClick = (imageId) => {
-  this.setState({modalOpen: true})
+  toggleModal = () => {
+    this.setState(prevState => ({ showModal: !prevState.showModal }));
+  };
 
-   const findId = this.state.articles.find(({id}) => id === imageId)
-    return this.setState({largeUrl: findId.largeImageURL})
- }
-
-
- 
+  handleImageClick = index => {
+    this.toggleModal();
+    return this.setState({ activIndex: this.state.articles[index] });
+  };
 
   render() {
+    const { showModal, activIndex, articles, page,  status } = this.state; // isloading
 
-  
-    return (
-      // <Box width="1200px" m="0 auto" p="0 20px">
-        <div className="App">
-          {this.state.modalOpen &&  <Modal url={this.state.largeUrl} />}
-          <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={this.state.page}/>
-          {/* <ImageGallery query={this.state.query} page={this.state.page} /> */}
-          <ImageGallery articles={this.state.articles} onImageClick={this.handleImageClick} />
-          {this.state.articles.length > 0 && <Button onClickBtnIncrement={this.handlebtnIncrement} />}
-        </div>
-      // </Box>
-    );
+      if (status === 'idle') {
+        return (
+          <div className="App" >
+          <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
+          </div>
+        );
+      }
+
+      if (status === 'pending') {
+        return (
+          <div className="App">
+            <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
+            <Circles color="#ff0095" height={80} width={80} />
+            <ImageGallery articles={articles} onImageClick={this.handleImageClick} />
+          </div>
+        );
+      }
+
+      if (status === 'resolved') {
+        return (
+          <div className="App">
+            <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
+            <ImageGallery articles={articles} onImageClick={this.handleImageClick} />
+            <Button onClickIncrementBtn={this.handleBtnIncrement} />
+            {showModal && (
+            <Modal onCloseModal={this.toggleModal}>
+              <img src={activIndex.largeImageURL} alt={activIndex.tags} />
+            </Modal>
+            )}
+          </div>
+        );
+      }
+
+      
+    
+    
+    // return (
+    //   // <Box width="1200px" m="0 auto" p="0 20px">
+    //   <div className="App">
+    //     <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
+    //     {/* {isloading && <Circles color="#ff0095" height={80} width={80} />} */}
+    //     <ImageGallery
+    //       articles={articles}
+    //       onImageClick={this.handleImageClick}
+    //     />
+    //     {articles.length > 0 && (
+    //       <Button onClickIncrementBtn={this.handleBtnIncrement} />
+    //     )}
+    //     {showModal && (
+    //       <Modal onCloseModal={this.toggleModal}>
+    //         <img src={activIndex.largeImageURL} alt={activIndex.tags} />
+    //       </Modal>
+    //     )}
+    //   </div>
+    //   // </Box>
+    // );
   }
 }
 
