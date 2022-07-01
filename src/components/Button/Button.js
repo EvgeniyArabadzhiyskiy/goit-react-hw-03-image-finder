3f44c67b51fd9920f8fbdc6ee837fcb749155334
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-01 18:48:55 +0300

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,13 @@
-const Button = ({onClickBtnIncrement}) => {
+import PropTypes from "prop-types";
+
+const Button = ({onClickIncrementBtn}) => {
     return ( <div>
-        <button className="Button" type="buttton" onClick={onClickBtnIncrement}>Load more</button>
+        <button className="Button" type="buttton" onClick={onClickIncrementBtn}>Load more</button>
     </div> );
+}
+
+Button.propTypes = {
+    onClickIncrementBtn: PropTypes.func.isRequired,
 }
  
 export default Button;
