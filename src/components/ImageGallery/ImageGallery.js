b0bfb294f1eb14a471f0284002a00ff5f658493d
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-01 18:49:04 +0300

    Update ImageGallery.js

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,45 +1,21 @@
-// import axios from 'axios';
-// import Button from 'components/Button/Button';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Component } from 'react';
 
-class ImageGallery extends Component {
-  state = {
-    // query: '',
-    // articles: [],
-    // page: 1,
-  };
-
-  
-
-//   async componentDidUpdate(prevProps, prevState) {
-
-
-//     if (this.props.query !== prevProps.query || this.props.page !== prevProps.page) {
-//         // console.log(this.state.page);
-//         // this.setState({query: this.props.query})
-//       const response = await axios(
-//         `https://pixabay.com/api/?key=26298929-dc8db63efad38f2c4177a32d6&q=${this.props.query}&image_type=photo&page=${this.props.page}&per_page=12`
-//       );
-
-//         this.setState(prevState => ({articles: [...prevState.articles, ...response.data.hits]}))
-//     }
-//   }
-
-//   handlebtnIncrement  = (e) => {
-//      this.setState(prevState => ({page: prevState.page + 1}))
-//   };
-
-
-  render() {
-    return <ul className='ImageGallery'>
-        {this.props.articles.map(({id, tags, webformatURL}) => {
-           return <ImageGalleryItem key={id} id={id} tags={tags} ImgUrl={webformatURL} onImageClick={this.props.onImageClick} />
-        })}
-         
-        {/* { this.state.articles.length > 0 && <Button onClickBtnIncrement={this.handlebtnIncrement} />} */}
-    </ul>;
-  }
-}
+const ImageGallery = ({ articles, onImageClick }) => {
+  return (
+    <ul className="ImageGallery">
+      {articles.map(({ id, tags, webformatURL }, index) => {
+        return (
+          <ImageGalleryItem
+            key={id}
+            index={index}
+            tags={tags}
+            ImgUrl={webformatURL}
+            onImageClick={onImageClick}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
 export default ImageGallery;
