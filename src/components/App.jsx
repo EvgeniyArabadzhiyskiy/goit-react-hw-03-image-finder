c082fa85e3ad0c71976816f63bf8f59d29336cd1
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-02 20:58:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
-// import axios from 'axios';
-import { Circles } from 'react-loader-spinner';
 import { Component } from 'react';
-import Button from './Button/Button';
-// import Box from './Box/Box';
-import ImageGallery from './ImageGallery/ImageGallery';
-import Searchbar from './Searchbar/Searchbar';
+import { ToastContainer, toast } from 'react-toastify';
+import { Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Box from './Box/Box';
 import Modal from './Modal/Modal';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
 import fetchImages from 'service/pixabay-api';
 
 class App extends Component {
@@ -15,28 +18,33 @@ class App extends Component {
     articles: [],
     showModal: false,
     activIndex: null,
-    // isloading: false,
-    status: 'idle',
+    isloading: false,
+    error: null,
   };
 
-  async componentDidUpdate(prevProps, prevState) {
+  async componentDidUpdate(_, prevState) {
     const searchQuery = this.state.query;
     const searchPage = this.state.page;
 
     if (searchQuery !== prevState.query || searchPage !== prevState.page) {
-      // this.setState({ isloading: true });
-      this.setState({ status: 'pending' });
-
+      
       try {
+        this.setState({ isloading: true });
         const imagesHits = await fetchImages(searchQuery, searchPage);
 
-        this.setState(prevState => ({
-          articles: [...prevState.articles, ...imagesHits],
-          status: 'resolved'
-        }));
-      } catch (error) {console.log(error)} 
-      // finally {this.setState({ isloading: false })}
+        if (imagesHits.length === 0) {
+          toast.warning("No results were found for your search, please try something else.",
+           {transition: Zoom})
+          return
+        }
 
+        this.setState(({articles}) => ({articles: [...articles, ...imagesHits]}));
+
+      } catch (error) {
+        this.setState({ error: new Error("Sorry something went wrong")});
+      } finally {
+        this.setState({ isloading: false });
+      }
     }
   }
 
@@ -44,7 +52,7 @@ class App extends Component {
     this.setState({ query: text.trim(), page: 1, articles: [] });
   };
 
-  handleBtnIncrement = e => {
+  handleLoadMore = e => {
     this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
@@ -57,69 +65,52 @@ class App extends Component {
     return this.setState({ activIndex: this.state.articles[index] });
   };
 
+  
+
   render() {
-    const { showModal, activIndex, articles, page, status } = this.state; // isloading status
-
-
-    if (status === 'idle') {
-      return (
-        <div className="App" >
-        <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
-        </div>
-      );
+    const { showModal, activIndex, articles, page, isloading, error } = this.state; 
+   
+    if (error) {
+      toast.error(this.state.error.message)
     }
-    
-    if (status === 'pending') {
-      return (
-        <div className="App">
-          <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
-          <Circles color="#ff0095" height={80} width={80} />
-          <ImageGallery articles={articles} onImageClick={this.handleImageClick} />
-        </div>
-      );
-    }
-    
-    if (status === 'resolved') {
-      return (
-        <div className="App">
-          <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
-          <ImageGallery articles={articles} onImageClick={this.handleImageClick} />
-          <Button onClickIncrementBtn={this.handleBtnIncrement} />
-          {showModal && (
+
+    return (
+  
+      <Box display="grid" gridAutoColumns="1fr" gridGap="16px" pb={6} >
+        <Searchbar  onSearhFormSubmit={this.handleFormSubmit} page={page} />
+        
+        {isloading && <Loader />}
+        
+        {articles.length > 0 && (
+        <>
+          <ImageGallery articles={articles}  onImageClick={this.handleImageClick} />
+          <Button onLoadMore={this.handleLoadMore} /> 
+        </>)}
+        
+        {showModal && (
           <Modal onCloseModal={this.toggleModal}>
             <img src={activIndex.largeImageURL} alt={activIndex.tags} />
           </Modal>
-          )}
-        </div>
-      );
-    }
+        )}
+        
+        <ToastContainer autoClose={3000}  position="top-center" theme="colored" pauseOnHover  />
+      </Box>
+      
+    );
 
-
-
-    // return (
-    //   // <Box width="1200px" m="0 auto" p="0 20px">
-    //   <div className="App">
-    //     <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
-    //     {isloading && <Circles color="#ff0095" height={80} width={80} />}
-    //     <ImageGallery
-    //       articles={articles}
-    //       onImageClick={this.handleImageClick}
-    //     />
-    //     {articles.length > 0 && (
-    //       <Button onClickIncrementBtn={this.handleBtnIncrement} />
-    //     )}
-    //     {showModal && (
-    //       <Modal onCloseModal={this.toggleModal}>
-    //         <img src={activIndex.largeImageURL} alt={activIndex.tags} />
-    //       </Modal>
-    //     )}
-    //   </div>
-    //   // </Box>
-    // );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
 
 // if (status === 'idle') {
 //   return (
