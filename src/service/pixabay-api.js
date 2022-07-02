68b5fdc4e7043c66d2c25f559e4ff0b21323520f
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-02 21:00:43 +0300

    Update pixabay-api.js

--- src/service/pixabay-api.js
+++ src/service/pixabay-api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '26298929-dc8db63efad38f2c4177a32d6';
+const OPTIONS = 'image_type=photo';
 
 async function fetchImages(query, page) {
   const response = await axios(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&page=${page}&per_page=12`
+    `${BASE_URL}?key=${API_KEY}&q=${query}&${OPTIONS}&page=${page}&per_page=12`
   );
 
   return response.data.hits;
