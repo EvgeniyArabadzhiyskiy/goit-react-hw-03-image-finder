ab3c78491107b4627595ae5cbb2f0cebc9a0bc4b
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-06 19:31:18 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,37 +21,44 @@ class App extends Component {
     activIndex: null,
     isloading: false,
     error: null,
+    status: 'idle',
   };
 
-   totalHits = null
+  totalHits = null;
 
   async componentDidUpdate(_, prevState) {
     const searchQuery = this.state.query;
     const searchPage = this.state.page;
 
     if (searchQuery !== prevState.query || searchPage !== prevState.page) {
-      
       try {
-        this.setState({ isloading: true });
+        // this.setState({ isloading: true });
+        this.setState({ status: 'pending' });
         const imageData = await fetchImages(searchQuery, searchPage);
 
-        this.totalHits = imageData.total
+        this.totalHits = imageData.total;
 
         const imagesHits = imageData.hits;
-        
+
         if (imagesHits.length === 0) {
-          toast.warning("No results were found for your search, please try something else.",
-           {transition: Zoom})
-          return
+          toast.warning(
+            'No results were found for your search, please try something else.',
+            { transition: Zoom }
+          );
+          return;
         }
 
-        this.setState(({articles}) => ({articles: [...articles, ...imagesHits]}));
-
+        this.setState(({ articles }) => ({
+          articles: [...articles, ...imagesHits],
+          status: 'resolved',
+        }));
       } catch (error) {
-        this.setState({ error: new Error("Sorry something went wrong")});
-      } finally {
-        this.setState({ isloading: false });
+        this.setState({
+          error: new Error('Sorry something went wrong'),
+          status: 'rejected',
+        });
       }
+      // finally {this.setState({ isloading: false });}
     }
   }
 
@@ -72,85 +79,87 @@ class App extends Component {
     return this.setState({ activIndex: this.state.articles[index] });
   };
 
-  
-
   render() {
-    const { showModal, activIndex, articles, page, isloading, error } = this.state; 
-    const countImages = articles.length
-   
+    const { showModal, activIndex, articles, page,  error, status } = this.state;  //isloading
+    const countImages = articles.length;
+
     if (error) {
-      toast.error(this.state.error.message)
+      toast.error(this.state.error.message);
     }
 
-    return (
-  
-      <Box display="grid" gridAutoColumns="1fr" gridGap="16px" pb={6} >
-        <Searchbar  onSearhFormSubmit={this.handleFormSubmit} page={page} />
-        
-        {isloading && <Loader />}
-        
-        {countImages > 0 && <ImageGallery articles={articles}  onImageClick={this.handleImageClick} />}
+    if (status === 'idle') {
+      return (
+        <Box display="grid" gridAutoColumns="1fr" gridGap="16px" pb={6}>
+          <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
+        </Box>
+      );
+    }
 
-        { (countImages > 0 && countImages !== this.totalHits) && <Button onLoadMore={this.handleLoadMore} />}
+    if (status === 'pending') {
+      return (
+        <Box display="grid" gridAutoColumns="1fr" gridGap="16px" pb={6}>
+          <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
+           <Loader />
+          <ImageGallery
+            articles={articles}
+            onImageClick={this.handleImageClick}
+          />
+          <ToastContainer autoClose={3000}  position="top-center" theme="colored" pauseOnHover  />
+        </Box>
+      );
+    }
 
-        {countImages === this.totalHits && <Notification /> }
+    if (status === 'resolved') {
+      return (
+        <Box display="grid" gridAutoColumns="1fr" gridGap="16px" pb={6}>
+          <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
+          {countImages > 0 && (
+            <ImageGallery
+              articles={articles}
+              onImageClick={this.handleImageClick}
+            />
+          )}
 
-        {showModal && (
-          <Modal onCloseModal={this.toggleModal}>
-            <img src={activIndex.largeImageURL} alt={activIndex.tags} />
-          </Modal>
-        )}
+          {countImages > 0 && countImages !== this.totalHits && (
+            <Button onLoadMore={this.handleLoadMore} />
+          )}
+          {countImages === this.totalHits && <Notification /> }
+          {showModal && (
+            <Modal onCloseModal={this.toggleModal}>
+              <img src={activIndex.largeImageURL} alt={activIndex.tags} />
+            </Modal>
+          )}
+          
+        </Box>
+      );
+    }
 
-        <ToastContainer autoClose={3000}  position="top-center" theme="colored" pauseOnHover  />
-      </Box>
-      
-    );
+    // return (
 
+    //   <Box display="grid" gridAutoColumns="1fr" gridGap="16px" pb={6} >
+    //     <Searchbar  onSearhFormSubmit={this.handleFormSubmit} page={page} />
+
+    //     {isloading && <Loader />}
+
+    //     {countImages > 0 && <ImageGallery articles={articles}  onImageClick={this.handleImageClick} />}
+
+    //     { (countImages > 0 && countImages !== this.totalHits) && <Button onLoadMore={this.handleLoadMore} />}
+
+    //     {countImages === this.totalHits && <Notification /> }
+
+    //     {showModal && (
+    //       <Modal onCloseModal={this.toggleModal}>
+    //         <img src={activIndex.largeImageURL} alt={activIndex.tags} />
+    //       </Modal>
+    //     )}
+
+    //     <ToastContainer autoClose={3000}  position="top-center" theme="colored" pauseOnHover  />
+    //   </Box>
+
+    // );
   }
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-// if (status === 'idle') {
-//   return (
-//     <div className="App" >
-//     <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
-//     </div>
-//   );
-// }
-
-// if (status === 'pending') {
-//   return (
-//     <div className="App">
-//       <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
-//       <Circles color="#ff0095" height={80} width={80} />
-//       <ImageGallery articles={articles} onImageClick={this.handleImageClick} />
-//     </div>
-//   );
-// }
-
-// if (status === 'resolved') {
-//   return (
-//     <div className="App">
-//       <Searchbar onSearhFormSubmit={this.handleFormSubmit} page={page} />
-//       <ImageGallery articles={articles} onImageClick={this.handleImageClick} />
-//       <Button onClickIncrementBtn={this.handleBtnIncrement} />
-//       {showModal && (
-//       <Modal onCloseModal={this.toggleModal}>
-//         <img src={activIndex.largeImageURL} alt={activIndex.tags} />
-//       </Modal>
-//       )}
-//     </div>
-//   );
-// }
 
 
